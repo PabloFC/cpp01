8eda4c5fb5669e606da36bc2a1a47939e275cb3c
PabloFC <pablofc_68@hotmail.com>
2025-11-03 12:24:07 +0100

    adding headers in ex00

--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 12:21:22 by pafuente          #+#    #+#             */
+/*   Updated: 2025/11/03 12:21:22 by pafuente         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 
 // Creates a Zombie object on the stack with the given name
